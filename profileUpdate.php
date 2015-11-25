65d77c4a2aa3357041f2559c98fa7e9224548661
Tao <chentao@oregonstate.edu>
2015-11-24 18:58:46 -0800

    php for updating the user profile to database

---
+++ profileUpdate.php
@@ -0,0 +1,65 @@
+<!--This is the page that updates the user profile to the database-->
+<?php
+	session_start();
+	$userID = $_SESSION['uid'];
+	include 'dp.php';
+?>
+<!--seesion_start() must be the very first thing in the page-->
+
+<!DOCTYPE html>
+<html lang="US-EN">
+</head>
+	<title>Fitness Incentivizer Goal System</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+	
+	<link rel="stylesheet" type"text/css" media="screen" href="stylesheets/stylesheet.css">
+<body>
+
+	<!--HEADER-->
+	<div id="header_wrap" class="outer">
+		<header class="inner">
+			<h1 id="project_title">Fitness Incentivizer Goal System</h1>
+			<!-- <h2 id="project_tagline">Repository for team fig </h2> -->
+			<!--
+			<section id="downloads">
+			<a class="zip_download_link" href="https://github.com/ikaikastine/team-fig/zipball/master">Download this project as a .zip file</a>
+			<a class="tar_download_link" href="https://github.com/ikaikastine/team-fig/tarball/master">Download this project as a tar.gz file</a>
+			</section>
+			-->
+		</header>
+	</div>
+
+	<?php
+		//get the managerID and the password via POST
+		$firstName = $_POST['firstName'];
+		$lastName = $_POST['lastName'];
+		$age = $_POST['age'];
+		$weight = $_POST['weight'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+
+		//connect to the database to find the managerID and check if the password matches
+		$dbc = dbConnect();
+		$query = "UPDATE FIG_USER SET firstname = '$firstname', lastname = '$lastname', age = '$age', email = '$email', weight = '$weight', password = '$password';  WHERE user_id = '$userID'";
+		$result = mysqli_query($dbc, $query) or die(mysqli_error($dbc));
+		//terminate the connection with the database
+		mysqli_close($dbc);
+	?>
+
+	<!-- FOOTER -->
+	<div id="footer_wrap" class="outer">
+		<footer class="inner">
+			<p class="copyright">Fitness Incentivizer Goal System is maintained by Team-Fig</p>
+			<p>For more information click <a href="https://github.com/ikaikastine/team-fig">here</a></p>
+		</footer>
+	</div>
+
+
+</body>
+</html>
