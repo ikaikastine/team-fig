f2961d389747e3d70a3a5db79f7d22b018b7a93a
morgaalb <morgaalb@oregonstate.edu>
2015-12-01 19:32:04 -0800

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -8,10 +8,16 @@ DROP TABLE IF EXISTS FIG_WORKOUT;
 CREATE TABLE FIG_USER
 (
 	user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,   -- PK
-	name VARCHAR(100),      -- name of the user
-	username VARCHAR(100),  -- what they use to login with
-	password VARCHAR(100),  -- password (SHA2)
-	points INT UNSIGNED,	-- how many points do I have
+	name VARCHAR(100) NOT NULL,      -- name of the user
+	username VARCHAR(100) NOT NULL,  -- what they use to login with
+	password VARCHAR(100) NOT NULL,  -- password (SHA2)
+	points INT UNSIGNED NOT NULL DEFAULT 0,	-- how many points do I have
+	
+	picture BLOB, -- should we store the pic in the db or just store the path?
+	weight INT,
+	height INT,
+	age INT,
+	location VARCHAR(100),
 	
 	PRIMARY KEY(user_id)
 );
@@ -21,8 +27,7 @@ CREATE TABLE FIG_EXERCISE
 	eid INT UNSIGNED NOT NULL AUTO_INCREMENT, -- unique exercise id
 	name VARCHAR(100),      -- name of the exercise
 	kcalhr INT UNSIGNED,    -- how many calories it burns
-	description TEXT,       -- description of the exercise
-	
+
 	PRIMARY KEY(eid)
 );
 
@@ -32,6 +37,7 @@ CREATE TABLE FIG_WORKOUT
 	user_id INT UNSIGNED,   -- CK and FK to FIG_USER
 	exercise INT UNSIGNED,  -- CK and FK to FIG_EXERCISE
 	performed_on DATETIME,  -- when the user did the exercise
+	notes TEXT,       -- user notes	
 	
 	minutes INT UNSIGNED,   -- Number of minutes the workout lasted
 	
@@ -48,3 +54,12 @@ CREATE TABLE FIG_WORKOUT
 		ON UPDATE CASCADE
 );
 
+INSERT INTO FIG_EXERCISE (name, kcalhr) VALUES
+('Golf', 368),
+('Fencing', 490),
+('Cleaning gutters', 409),
+('Ballroom dancing', 401),
+('Archery', 409),
+('Bird watching', 204),
+('Water polo', 817),
+('Squash', 981);
