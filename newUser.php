e6db038c2233461fd0af21f931d6ee27bb549e42
Tao <chentao@oregonstate.edu>
2015-12-01 18:36:34 -0800

    added some indentation

--- newUser.php
+++ newUser.php
@@ -1,139 +1,134 @@
 <?php ini_set('display_errors', 'On'); ?>
 
 <?php // RegisterUser.php
-include 'common.php';
-include 'db.php';
-include_once 'cred.php';
-if (!isset($_POST['submitok'])):
-// Display the user signup form
+	include 'common.php';
+	include 'db.php';
+	include_once 'cred.php';
+	if (!isset($_POST['submitok'])):
+	// Display the user signup form
 ?>
 
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset='utf-8'>
+	<meta http-equiv="X-UA-Compatible" content="chrome=1">
+	<meta name="description" content="Team-fig : Repository for team fig ">
 
+	<link rel="stylesheet" type="text/css" media="screen" href="stylesheets/stylesheet.css">
 
-  <head>
-    <meta charset='utf-8'>
-    <meta http-equiv="X-UA-Compatible" content="chrome=1">
-    <meta name="description" content="Team-fig : Repository for team fig ">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
 
-    <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/stylesheet.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+	<title>Fitness Incentivizer Goal System</title>
+</head>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<body>
+	<!-- HEADER -->
+	<div id="header_wrap" class="outer">
+		<header class="inner">
+			<h1 id="project_title">Fitness Incentivizer Goal System</h1>
+		</header>
+	</div>
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
+	<!-- MAIN CONTENT -->
+	<div id="main_content_wrap" class="outer">
+		<section id="main_content" class="inner">
+			<h3><a id="welcome-to-github-pages" class="anchor" href="#welcome-to-github-pages" aria-hidden="true"><span class="octicon octicon-link"></span></a>Welcome New User!</h3>
 
-    <title>Fitness Incentivizer Goal System</title>
-  </head>
+			<p>Were excited that you've decided to join us. Please fill out the information below to complete the setup of your profile</p>
 
-  <body>
+			<form role="form" method="post" action="<?=$_SERVER['PHP_SELF']?>">
+				<div class="inner">
+					<div class="row">
+						<div class="form-group col-md-6">
+							<label for="name">Name</label>
+							<input class="form-control" name="name" placeholder="Name">
+						</div>
 
-    <!-- HEADER -->
-    <div id="header_wrap" class="outer">
-        <header class="inner">
-          <h1 id="project_title">Fitness Incentivizer Goal System</h1>
-        </header>
-    </div>
+						<div class="form-group col-md-6">
+							<label for="userID">Username</label>
+							<input class="form-control" name="userID" placeholder="Username">
+						</div>
+					</div>
 
-    <!-- MAIN CONTENT -->
-    <div id="main_content_wrap" class="outer">
-      <section id="main_content" class="inner">
-        <h3>
-        <a id="welcome-to-github-pages" class="anchor" href="#welcome-to-github-pages" aria-hidden="true"><span class="octicon octicon-link"></span></a>Welcome New User!</h3>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input class="form-control" type="password" name="password" placeholder="Password">
+					</div>
 
-            <p>Were excited that you've decided to join us. Please fill out the information below to complete the setup of your profile</p>
+					<button type="submit" name="submitok" class="btn btn-default">Submit</button>
+ 				</div>
+			</form>
+            <section>
+	</div>
 
-            <form role="form" method="post" action="<?=$_SERVER['PHP_SELF']?>">
-              <div class="inner">
-                <div class="row">
-                  <div class="form-group col-md-6">
-                    <label for="name">Name</label>
-                    <input class="form-control" name="name" placeholder="Name">
-                  </div>
-
-                  <div class="form-group col-md-6">
-                    <label for="userID">Username</label>
-                    <input class="form-control" name="userID" placeholder="Username">
-                  </div>
-                </div>
-
-                <div class="form-group">
-                  <label for="password">Password</label>
-                  <input class="form-control" type="password" name="password" placeholder="Password">
-                </div>
-
-                <button type="submit" name="submitok" class="btn btn-default">Submit</button>
-              </div>
-            </form>
-            </div>
-    </div>
-  </form>
-    <!-- FOOTER -->
-    <div id="footer_wrap" class="outer">
-      <footer class="inner">
-        <p class="copyright">Fitness Incentivizer Goal System is maintained by Team-Fig</p>
-        <p>For more information click <a href="https://github.com/ikaikastine/team-fig">here</a></p>
-      </footer>
-    </div>
+	<!-- FOOTER -->
+	<div id="footer_wrap" class="outer">
+		<footer class="inner">
+			<p class="copyright">Fitness Incentivizer Goal System is maintained by Team-Fig</p>
+			<p>For more information click <a href="https://github.com/ikaikastine/team-fig">here</a></p>
+		</footer>
+	</div>
 
 </body>
 </html>
 
 <?php
-  else:
-  // Process signup submission
-  $link = dbConnect();
-  if ($_POST['userID']=='' or $_POST['name']=='' or $_POST['password']=='') {
-    error('One or more required fields were left blank.\n'.
-    'Please fill them in and try again.');
-  }
+	else:
+		// Process signup submission
+		$link = dbConnect();
+		if ($_POST['userID']=='' or $_POST['name']=='' or $_POST['password']=='') {
+			error('One or more required fields were left blank.\n'. 
+				'Please fill them in and try again.');
+		}
 
 
-  // Check for existing user with the new id
-  $sql = "SELECT COUNT(*) FROM FIG_USER WHERE username = '$_POST[userID]'";
-  $result = mysqli_query($link,$sql);
-  if (!$result) {
-    error('A database error occurred in processing your '.
-    'submission.\nIf this error persists, please '.
-    'contact you@example.com.');
-  }
+		// Check for existing user with the new id
+		$sql = "SELECT COUNT(*) FROM FIG_USER WHERE username = '$_POST[userID]'";
+		$result = mysqli_query($link,$sql);
+		if (!$result) {
+			error('A database error occurred in processing your '.
+				'submission.\nIf this error persists, please '.
+				'contact you@example.com.');
+		}
 
-  //need to find a way to replace this to prevent duping users
-  //original code uses some ancient library that was deprecated
-  //need to convert to mysqli now
-  //im not a php wizard yet so... i haven't figured it out yet
+		//need to find a way to replace this to prevent duping users
+		//original code uses some ancient library that was deprecated
+		//need to convert to mysqli now
+		//im not a php wizard yet so... i haven't figured it out yet
 
-  /* if (@mysql_result($result,0,0)>0) {
-  error('A user already exists with your chosen userid.\n'.
-  'Please try another.');
-  } */
+		/* if (@mysql_result($result,0,0)>0) {
+		error('A user already exists with your chosen userid.\n'.
+		'Please try another.');
+		} */
 
-  $hash = hash('sha256', $_POST[password]);
+		$hash = hash('sha256', $_POST[password]);
 
-  $sql = "INSERT INTO FIG_USER SET
-  name = '$_POST[name]',
-  username = '$_POST[userID]',
-  password = '$hash'";
+		$sql = "INSERT INTO FIG_USER SET
+		name = '$_POST[name]',
+		username = '$_POST[userID]',
+		password = '$hash'";
 
-  if (!mysqli_query($link,$sql))
-    error('A database error occurred in processing your '.
-    'submission.\nIf this error persists, please '.
-    'contact you@example.com.');
+		if (!mysqli_query($link,$sql))
+			error('A database error occurred in processing your '.
+    				'submission.\nIf this error persists, please '.
+				'contact you@example.com.');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <title> Registration Complete </title>
-  <meta http-equiv="Content-Type"
-  content="text/html; charset=iso-8859-1" />
+	<title> Registration Complete </title>
+	<meta http-equiv="Content-Type"
+	content="text/html; charset=iso-8859-1" />
 </head>
 <body>
-  <p><strong>User registration successful!</strong></p>
-  <p>To log in, click <a href="index.php">here</a> to return to the main page.</p>
+	<p><strong>User registration successful!</strong></p>
+	<p>To log in, click <a href="index.php">here</a> to return to the main page.</p>
 </body>
 </html>
 <?php
